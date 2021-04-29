c9b150ceca4e57c137409ed763fa62b4ab8d96c7
hassanul50 <hasanakib27@gmail.com>
2021-04-29 19:41:01 +0600

    initial commit

---
+++ studentApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace studentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi . this is practice application for student information for asp.netcore corse");
+        }
+    }
+}
